2127b90cbae3c68921dcf3f477b5e7709daa6eee
aadimach2 <aadimach2@example.com>
2024-04-16 06:07:46 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div classname="home">
+        <main>
+            <h1> TechStar</h1>
+            <p>Soluton to all your problems</p>
+        </main>
+    </div>
+  )
+}
+
+export default Home
