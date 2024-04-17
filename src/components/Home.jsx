3caf3cf3c29a227ae4a36ff6d67c141378fa852d
aadimach2 <aadimach2@example.com>
2024-04-17 07:05:01 +0530

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import vg from "../assets/2.webp"
 
 const Home = () => {
   return (
-    <div classname="home">
+  <>
+    <div className="home">
         <main>
             <h1> TechStar</h1>
             <p>Soluton to all your problems</p>
         </main>
     </div>
+    <div className="home2">
+      <img src={vg} alt='Graphics'/>
+      <div>
+        <p>
+          we are your one and only solution to tech problems
+        </p>
+      </div>
+
+    </div>
+  </>
   )
 }
 
